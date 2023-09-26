de563864da79c6e977a0f335b9a0ed0637682a77
c0b22025 <c0b2202591@edu.teu.ac.jp>
2023-09-26 15:33:39 +0900

    こうかとん

---
+++ pygame_image.py
@@ -0,0 +1,32 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+
+    tori = pg.image.load("fig/3.png")
+    tori=pg.transform.flip(tori, True, False)
+    
+    
+    
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(tori,[300,200])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
