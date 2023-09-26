ec8a719f875eb16b3b3b10021382a426214de360
c0b22025 <c0b2202591@edu.teu.ac.jp>
2023-09-26 17:50:02 +0900

    工科トンんん

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,8 @@ def main():
 
     tori = pg.image.load("fig/3.png")
     tori=pg.transform.flip(tori, True, False)
+    bg_img2=pg.transform.flip(bg_img, True, False)
+
 
     tori_move1=pg.transform.rotozoom(tori,2,1.0)
     tori_move2=pg.transform.rotozoom(tori,4,1.0)
@@ -24,20 +26,23 @@ def main():
 
     tories=[tori,tori_move1,tori_move2,tori_move3,tori_move4,tori_move5,tori_move6,tori_move7,tori_move8,tori_move9,tori_move10]
     
-
     tmr = 0
+    cut=0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x=tmr%1600
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img,[1600-x,0])
+        screen.blit(bg_img, [-cut, 0])
+        screen.blit(bg_img, [3200-cut, 0])
+        screen.blit(bg_img2,[1600-cut,0])
         screen.blit(tories[tmr%11],[300,200])
-     
         pg.display.update()
         tmr += 1
+        cut+=1
+
+        if cut%3200 ==0:
+            cut=0
      
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
