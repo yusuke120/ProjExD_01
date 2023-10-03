79591820f6002502100cdc2aaee82d2609da02a8
c0b22025 <c0b2202591@edu.teu.ac.jp>
2023-10-03 15:07:51 +0900

    休み時間

--- pygame_image.py
+++ pygame_image.py
@@ -1,48 +1,62 @@
+import random
 import sys
 import pygame as pg
 
 
+WIDTH, HEIGHT = 1600, 900
+
+
+delta = {  # 練習３：移動量辞書
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
+
+
 def main():
-    pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((800, 600))
-    clock  = pg.time.Clock()
+    pg.display.set_caption("逃げろ！こうかとん")
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    """こうかとん"""
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900, 400)  # 練習３：こうかとんの初期座標を設定する
+    """ばくだん"""
+    bd_img = pg.Surface((20, 20))  # 練習１：爆弾Surfaceを作成する
+    bd_img.set_colorkey((0, 0, 0))  # 練習１：黒い部分を透明にする
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_rct = bd_img.get_rect()  # 練習１：SurfaceからRectを抽出する
+    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    bd_rct.center = (x, y)  # 練習１：Rectにランダムな座標を設定する
+    vx, vy = +5, +5  # 練習２：爆弾の速度
 
-    tori = pg.image.load("fig/3.png")
-    tori=pg.transform.flip(tori, True, False)
-    bg_img2=pg.transform.flip(bg_img, True, False)
-
-
-    tori_move1=pg.transform.rotozoom(tori,2,1.0)
-    tori_move2=pg.transform.rotozoom(tori,4,1.0)
-    tori_move3=pg.transform.rotozoom(tori,6,1.0)
-    tori_move4=pg.transform.rotozoom(tori,8,1.0)
-    tori_move5=pg.transform.rotozoom(tori,10,1.0)
-    tori_move6=pg.transform.rotozoom(tori,8,1.0)
-    tori_move7=pg.transform.rotozoom(tori,6,1.0)
-    tori_move8=pg.transform.rotozoom(tori,4,1.0)
-    tori_move9=pg.transform.rotozoom(tori,2,1.0)
-    tori_move10=pg.transform.rotozoom(tori,0,1.0)
-
-    tories=[tori,tori_move1,tori_move2,tori_move3,tori_move4,tori_move5,tori_move6,tori_move7,tori_move8,tori_move9,tori_move10]
-    
+    clock = pg.time.Clock()
     tmr = 0
-    cut=0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
-        screen.blit(bg_img, [-cut, 0])
-        screen.blit(bg_img, [3200-cut, 0])
-        screen.blit(bg_img2,[1600-cut,0])
-        screen.blit(tories[tmr%11],[300,200])
+            if event.type == pg.QUIT: 
+                return
+            
+        screen.blit(bg_img, [0, 0])
+
+        """こうかとん"""
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for key, mv in delta.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]  # 練習３：横方向の合計移動量
+                sum_mv[1] += mv[1]  # 練習３：縦方向の合計移動量
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])  # 練習３：移動させる
+        screen.blit(kk_img, kk_rct)  # 練習３：移動後の座標に表示させる
+        """"ばくだん"""
+        bd_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
+        screen.blit(bd_img, bd_rct)  # 練習１：Rectを使って試しにblit
+        
         pg.display.update()
         tmr += 1
-        cut+=1
-
-        if cut%3200 ==0:
-            cut=0
-     
-        clock.tick(100)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
