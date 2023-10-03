7d3dd204a0c17b30143aa9077ede54fb2b8a7df7
c0b22025 <c0b2202591@edu.teu.ac.jp>
2023-10-03 15:29:41 +0900

    練習問題おわり

--- pygame_image.py
+++ pygame_image.py
@@ -14,12 +14,27 @@ delta = {  # 練習３：移動量辞書
 }
 
 
+def check_bound(obj_rct: pg.Rect):
+    """
+    引数：こうかとんRectかばくだんRect
+    戻り値：タプル（横方向判定結果，縦方向判定結果）
+    画面内ならTrue，画面外ならFalse
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right: # 横方向判定
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom: # 縦方向判定
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")
     """こうかとん"""
     kk_img = pg.image.load("fig/3.png")
+
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = (900, 400)  # 練習３：こうかとんの初期座標を設定する
@@ -38,7 +53,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-            
+
+        if kk_rct.colliderect(bd_rct):  # 練習５：ぶつかってたら
+            print("ゲームオーバー")
+            return
+
         screen.blit(bg_img, [0, 0])
 
         """こうかとん"""
@@ -49,9 +68,16 @@ def main():
                 sum_mv[0] += mv[0]  # 練習３：横方向の合計移動量
                 sum_mv[1] += mv[1]  # 練習３：縦方向の合計移動量
         kk_rct.move_ip(sum_mv[0], sum_mv[1])  # 練習３：移動させる
+        if check_bound(kk_rct) != (True, True):  # 練習４：はみだし判定
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) 
         screen.blit(kk_img, kk_rct)  # 練習３：移動後の座標に表示させる
         """"ばくだん"""
         bd_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:  # 練習４：横方向にはみ出たら
+            vx *= -1
+        if not tate:  # 練習４：縦方向にはみ出たら
+            vy *= -1
         screen.blit(bd_img, bd_rct)  # 練習１：Rectを使って試しにblit
         
         pg.display.update()
